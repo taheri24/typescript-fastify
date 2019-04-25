c76c62ed41972ee3ce46298181ef808ae4956eff
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2019-04-25 23:17:09 +0430

    add typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 // Require the framework and instantiate it
 //const fastify = require()({ logger: true })
 import * as fastify from 'fastify';
-const app=fastify({logger:true});
+import { createConnection, Connection } from 'typeorm';
+import { User } from './entity/User';
+let defaultConn: Connection;
+const app = fastify({ logger: true });
 // Declare a route
 app.get('/', async (request, reply) => {
-     return { hello: 'DDD-taheri' }
+  const userRepo = defaultConn.getRepository(User);
+
+  return userRepo.findOne(1);
 });
 
 // Run the server!
 const start = async () => {
-    console.log('Starting Server With ProcessId>>',process.pid);
-    try {
-        await app.listen(3000)
+  defaultConn = await createConnection();
+
+  try {
+    await app.listen(3100)
     console.log(`server listening on ${JSON.stringify(app.server.address())}`)
   } catch (err) {
     app.log.error(err)
