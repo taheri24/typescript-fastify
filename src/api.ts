a232a637cb274033b650d7b5de935cfe272a6bef
Majid Taheri(I love optimization) <taheri24@users.noreply.github.com>
2019-04-30 21:16:07 +0430

    add .node-dev.json for disable notify(toast)

--- src/api.ts
+++ src/api.ts
@@ -3,8 +3,9 @@ import RolePlugin from './plugins/role';
 import UserPlugin from "./plugins/user";
  
 export default function ApiPlugin(fastify:FastifyInstance,opts,next){
+    // register business routes
     fastify.register(RolePlugin,{  prefix:'/roles'})
     fastify.register(UserPlugin,{  prefix:'/users'})
-
+    //  call next   
     next();
 }
--- src/core/fastify-plugin-for-crud.ts
+++ src/core/fastify-plugin-for-crud.ts
@@ -1,21 +1,33 @@
 import { defaultConn } from "./db";
 import { FastifyInstance } from "fastify";
 
-export default function FastPluginForCRUD<T>(classType: any){
-           
-    return function(fastify:FastifyInstance,opts,next){
-        const repo=defaultConn.getRepository(classType);
-        const relations=repo.metadata.relations.map(r=>r.propertyPath)
-             
-        fastify.get('/:id',async req=>{
-            const {id}=req.params;
-            const result=await repo.findOne(id,{relations});
-            return result || {message:'not found'};
-        })
-        fastify.post('/',async req=>{
-            return defaultConn.manager.save(classType,req.body);
-        })
-         
+export default function FastPluginForCRUD<T>(classType: any) {
+
+    return function (fastify: FastifyInstance, opts, next) {
+        const repo = defaultConn.getRepository(classType);
+        const relations = repo.metadata.relations.map(r => r.propertyPath);
+        fastify.get('/:id', async req => {
+            const { id } = req.params;
+            const result = await repo.findOne(id, { relations });
+            return result || { message: 'not found' };
+        });
+        fastify.get('/schema', async () => {
+            const { metadata } = repo;
+            const columns = metadata.columns.map(c => c.propertyName);
+            const { tableName } = metadata;
+            return { tableName, columns };
+        });
+        fastify.get('/', async req => {
+            const result = await repo.find();
+            return result || { message: 'not found' };
+        });
+        fastify.post('/', async req => {
+            const entity = repo.create();
+            Object.assign(entity, req.body);
+            const storedEntity = repo.save(entity);
+            const id = repo.getId(storedEntity);
+            return { id };
+        });
         next();
     }
 }
