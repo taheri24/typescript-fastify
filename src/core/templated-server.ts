fe82926c0f55b02d891649f7ff41a12fb7777b9c
majid_taheri <majid@taheri24.ir>
2019-05-05 14:17:00 +0430

    Update templated-server.ts

--- src/core/templated-server.ts
+++ src/core/templated-server.ts
@@ -3,12 +3,12 @@ import { FastifyInstance } from "fastify";
 export interface BaseTemplateDefinition {
     hooks?: any;
     model?: any;
-    inject(fastify: FastifyInstance,model,hooks)
+    inject(fastify: FastifyInstance, model, hooks)
 }
 export interface TemplateDefinition<M, THooks> {
     hooks?: THooks;
     model?: M;
-    inject(fastify: FastifyInstance,model,hooks);
+    inject(fastify: FastifyInstance, model, hooks);
 }
 
 
@@ -17,7 +17,7 @@ export default function templatedServer<T extends BaseTemplateDefinition>(templa
     model: T['model'],
     hooks?: T['hooks']) {
     return function (fastify: FastifyInstance, opts, next) {
-        template.inject(fastify,model,hooks);
+        template.inject(fastify, model, hooks);
         next();
     }
 } 
